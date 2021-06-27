60157ef2b8f97641a781af2bff185fb92406b744
hichem chatbi <hichem@courrier.agency>
2021-06-27 13:49:52 +0200

    importé les components post author postByTag AllPosts

--- src/router.js
+++ src/router.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
+import Post from '@/components/Post'
+import Author from '@/components/Author'
+import PostsByTag from '@/components/PostsByTag'
+import AllPosts from '@/components/AllPosts'
 
