ef07400db1f49570e8f55ee05b7fb656c2d125d7
hichem chatbi <hichem@courrier.agency>
2021-06-27 14:03:09 +0200

    ajouter des routes

--- src/router.js
+++ src/router.js
@@ -6,3 +6,11 @@ import Author from '@/components/Author'
 import PostsByTag from '@/components/PostsByTag'
 import AllPosts from '@/components/AllPosts'
 
+Vue.use(VueRouter)
+
+const routes = [
+    {path:'/author/:username', component:Author },
+    {path:'/post/:slug', component:Post },
+    {path:'/tag/:tag', component: PostsByTag },
+    {path:'/', component: AllPosts },
+]
